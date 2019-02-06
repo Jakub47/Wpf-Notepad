a7571b7f35f7f1bee3cb66fa2dbcb14f11326098
Jakub47 <jakub7447@gmail.com>
2019-02-06 15:59:42 +0100

    First code Dock + String formats + Menu protopty!

---
+++ Notepad/Notepad/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Notepad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtBasicInfo.Text = "Line 0 Char 0 Letters 0 Vowels a e i o u";
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            TextProp.LettersCount = txtMainArea.Text.Trim().Count();
+            TextProp.VowelCounter(txtMainArea.Text);
+            ChangeStatusInfo();
+        }
+
+        private void TextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            CursorProp.row = txtMainArea.GetLineIndexFromCharacterIndex(txtMainArea.CaretIndex);
+            CursorProp.col = txtMainArea.CaretIndex - txtMainArea.GetCharacterIndexFromLineIndex(CursorProp.row);
+            ChangeStatusInfo();
+
+            //txtBasicInfo.Text = "Line " + (row + 1) + ", Char " + (col + 1);
+        }
+
+        private void ChangeStatusInfo()
+        {
+            txtBasicInfo.Text = "Line " + (CursorProp.row + 1) + " Char " + (CursorProp.col + 1) + 
+                                " Letters " + txtMainArea.Text.Trim().Count() + " Vowels " + TextProp.Vowels;
+        }
+    }
+}
---
+++ Notepad/Notepad/TextProp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Notepad
+{
+    public static class TextProp
+    {
+        public static int WordCount;
+        public static int LettersCount;
+        public static int Palindroms;
+        public static string Vowels;
+
+        static void Palindrome(string text)
+        {
+            string reverseText = Reverse(text);
+            if (reverseText.Equals(text, StringComparison.CurrentCultureIgnoreCase))
+                Palindroms++;
+        }
+
+        private static string Reverse(string text)
+        {
+            StringBuilder tempValue = new StringBuilder();
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                tempValue.Append(text[i]);
+            }
+            return tempValue.ToString();
+        }
+
+        public static void VowelCounter(string temp)
+        {
+            Vowels = "";
+            string pattern = @"[a e i o u]";
+            Regex regex = new Regex(pattern);
+            Dictionary<char, int> vowels = new Dictionary<char, int>();
+            vowels.Add('a', 0); vowels.Add('e', 0); vowels.Add('i', 0); vowels.Add('o', 0); vowels.Add('u', 0);
+            vowels['a'] = vowels['a']++;
+            
+            //CountEveryOccurence
+            foreach (char letter in temp)
+            {
+                if (letter != ' ' && regex.IsMatch(letter.ToString()))
+                {
+                    vowels[letter] = vowels[letter] + 1;
+                }
+            }
+
+            foreach (var value in vowels)
+            {
+                Vowels += value.Key + ": " + value.Value;
+            }
+        }
+    }
+}
